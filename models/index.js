b3af81b58f6e697234abc95220ec7f3c6b1eca14
Darya <tarasevichdarya@yandex.by>
2018-12-19 20:04:49 +0300

    Add dir with models of pizza, turtles and weapons

---
+++ models/index.js
@@ -0,0 +1,49 @@
+const Turtle = require('./turtle');
+const Weapon = require('./weapon');
+const Pizza = require('./pizza');
+const config = require('../config.json');
+
+module.exports = (Sequelize, config) => {
+    //todo: create object for connecting to db-sequelize
+    const sequelize = new Sequelize(config.db,
+        config.login,
+        config.password,
+        {
+            host: config.host,
+            dialect: config.dialect,
+            logging: false
+        });
+    sequelize.authenticate()
+        .then(() => {
+            console.log('Success');
+        }).catch((err) => {
+        console.log('error in connecting', err);
+    })
+
+    const turtles = Turtle(Sequelize, sequelize);
+    const weapons = Weapon(Sequelize, sequelize);
+    const pizzas = Pizza(Sequelize, sequelize);
+
+    //todo: creating relations between tables
+    turtles.belongsTo(pizzas, {
+        foreignKey: 'firstFavouritePizzaId',
+        as: 'firstFavouritePizza'
+    });
+    turtles.belongsTo(pizzas, {
+        foreignKey: 'secondFavouritePizzaId',
+        as: 'secondFavouritePizza'
+    });
+    turtles.belongsTo(weapons, {
+        foreignKey: 'weaponId',
+        as: 'weapon'
+    })
+
+    return{
+        turtles,
+        weapons,
+        pizzas,
+
+        sequelize: sequelize,
+        Sequelize: Sequelize
+    };
+};
---
+++ models/weapon.js
@@ -0,0 +1,15 @@
+module.exports = (Sequelize, sequelize) => {
+    return sequelize.define('weapons', {
+        id: {
+            type: Sequelize.INTEGER,
+            autoIncrement: true,
+            primaryKey: true},
+        name: {
+            type: Sequelize.STRING(500),
+            allowNull: false},
+        dps: {
+            type: Sequelize.INTEGER,
+            allowNull: false
+        }
+    });
+};
